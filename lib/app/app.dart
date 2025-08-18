3c8aadfb5ca29bc9f84cde24b1fdfd1fd81566c0
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-08-18 10:48:07 -0400

    refactor: Restructuring the file structure in screens and separating them through navigation with buttons

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,23 @@
+// import 'package:flutter/material.dart';
+// import 'package:traductor_voz/presentation/screens/home/home_screen.dart';
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Traductor Voz a Voz',
+//       theme: ThemeData(primarySwatch: Colors.deepPurple),
+//       home: HomeScreen(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:traductor_voz/presentation/screens/home/home_screen.dart';
+import 'package:traductor_voz/presentation/screens/double_via_speak/double_via_speak_screen.dart';
+import 'package:traductor_voz/presentation/screens/uni_via_speak/uni_via_speak_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,8 +26,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Traductor Voz a Voz',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: HomeScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/doubleVia': (context) => DoubleViaSpeakScreen(),
+        '/uniVia': (context) => UniViaSpeakScreen(),
+      },
     );
   }
 }
